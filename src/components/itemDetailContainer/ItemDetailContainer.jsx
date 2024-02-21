e9b2172c80c16768d0a14e9b20a44101a3d069d0
pulibass <edu24689@gmail.com>
2024-02-21 19:50:22 -0300

    actualizando repositorio, agregando fondos y botones de volver atras

--- src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
 import ItemDetails from '../itemDetails/ItemDetails'
+import { Link } from 'react-router-dom'
 
 function ItemDetailContainer() {
 
@@ -23,6 +24,9 @@ function ItemDetailContainer() {
   return (
     <div className='itemDetailContainer'>
       <h2><FontAwesomeIcon icon={faBagShopping} style={{ color: "#FFD43B", fontSize: "30px", padding: "5px" }} />Seleccionaste el producto: <span>{itemProducto.name}</span></h2>
+      <Link className='btnAtrasDetail' to={'/'}>
+        <button className='btn  fw-bold buttonAtrasDetail'>Volver a Productos</button>
+      </Link>
       <div className='contenedorItemDetail'>
         <ItemDetails itemProductos={itemProducto} />
       </div>
